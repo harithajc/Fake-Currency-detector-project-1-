b77eb6ae91510572d2d3b4d67a6ebcdb84dfccc8
Haritha <harithajc.official@gmail.com>
2026-03-13 20:00:03 +0530

    Finding the Fingerprints (Keypoints & Descriptors)

--- detect.py
+++ detect.py
@@ -1,7 +1,17 @@
 #module OpenCV has an massive set of tools specifically built for computer vision and image processing
 import cv2
+
 #converting the image to grayscale so that lighting and colour dont matter much ,we are concentrating more on the shape and content 
 real_note = cv2.imread('real_note.jpg', cv2.IMREAD_GRAYSCALE)
 test_note = cv2.imread('test_note.jpg', cv2.IMREAD_GRAYSCALE)
+
 #Creating the "Magnifying Glass" (ORB).Instead of looking at a blank patch of paper, it specifically searches for "corners" and unique textures.
 orb = cv2.ORB_create()
+
+#passing both our real note and our test note through the ORB detector.
+
+#kp (Keypoints): These are the exact X and Y coordinates on the image where the algorithm found something unique
+kp1, des1 = orb.detectAndCompute(real_note, None)
+
+#des (Descriptors): This is the mathematical description of what that keypoint looks like.
+kp2, des2 = orb.detectAndCompute(test_note, None)
