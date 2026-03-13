b1092a546b1b0972759d61a2a197ae06fe87fb40
Haritha <harithajc.official@gmail.com>
2026-03-13 19:57:05 +0530

    s2

--- detect.py
+++ detect.py
@@ -1,2 +1,7 @@
 #module OpenCV has an massive set of tools specifically built for computer vision and image processing
 import cv2
+#converting the image to grayscale so that lighting and colour dont matter much ,we are concentrating more on the shape and content 
+real_note = cv2.imread('real_note.jpg', cv2.IMREAD_GRAYSCALE)
+test_note = cv2.imread('test_note.jpg', cv2.IMREAD_GRAYSCALE)
+#Creating the "Magnifying Glass" (ORB).Instead of looking at a blank patch of paper, it specifically searches for "corners" and unique textures.
+orb = cv2.ORB_create()
